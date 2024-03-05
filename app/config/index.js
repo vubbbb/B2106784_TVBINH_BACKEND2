d3156cedcd61311a32195e790812a3820049749c
Tran Vu Binh <tranb4010@gmail.com>
2024-03-05 20:18:35 +0700

    Cai dat cac handler truy xuat CSDL

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
     app: {
     port: process.env.PORT || 3000,
+    },
+    db: {
+    uri: process.env.MONGODB_URI || "mongodb://localhost:27017/contactbook"
     }
 };
 module.exports = config;
